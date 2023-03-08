9a7c7a4b0ee089043b9d7548b7befbbdc528c26d
Barry d'Hoine <barrydhoine@gmail.com>
2023-03-08 22:16:03 +0100

    Catch errors on model fetching

--- netlify/functions/models.ts
+++ netlify/functions/models.ts
@@ -3,9 +3,12 @@ import {Handler, HandlerEvent, HandlerContext} from "@netlify/functions";
 
 const handler: Handler = async (event : HandlerEvent, context : HandlerContext) => {
     const openai = new OpenAIApi(new Configuration({apiKey: process.env.OPENAI_API_KEY}));
-    const models = await openai.listModels();
-
-    return {statusCode: 200, body: models.data};
+    try {
+        const models = await openai.listModels();
+        return {statusCode: 200, body: JSON.stringify(models.data)};
+    } catch {
+        return {statusCode: 500, body: "unable to fetch models"}
+    }
 };
 
 export {
