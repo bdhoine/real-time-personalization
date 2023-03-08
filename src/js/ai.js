9ea9dc7308a295a23e52fdf8695bf03e34eead85
Barry d'Hoine <barrydhoine@gmail.com>
2023-03-08 23:11:47 +0100

    Add eslint and image generation

--- src/js/ai.js
+++ src/js/ai.js
@@ -1,12 +1,22 @@
 import { Configuration, OpenAIApi } from "openai";
 
+const IMAGE_SIZE = "1024x1024";
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-(async() => {
-    const models = await openai.listModels();
-    console.log(models.data);
-})();
+
+const generateImage = async (prompt) => {
+    const response = await openai.createImage({
+        prompt,
+        n: 1,
+        size: IMAGE_SIZE,
+    });
+
+    console.log(response.data);
+};
+
+generateImage("a computer programmer participating in a hackaton");
