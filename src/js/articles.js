27149b9bf2520e56546d65261f2816b015ce680f
Timothy Baert <timothy.baert@amplexor.com>
2023-03-09 15:36:57 +0100

    Add article object with html, add 'with html' in the promt Generator

---
+++ src/js/articles.js
@@ -0,0 +1,35 @@
+const articles = {
+    article1: {
+        category: "Article",
+        content: "<p>Figuring out how to create a daily routine that works for you&mdash;and how to stick to it&mdash;can take some time. What should your perfect daily routine contain? That depends on your needs. Figure out what you have to get done and when. You also need to be honest about your lifestyle, including your bandwidth and time-management abilities, to create a reasonable work routine you can stick to.<br>\n" +
+            "            Here are five steps to help you make a daily routine for yourself, even if you've never had one.<br>\n" +
+            "            Make a List<br>\n" +
+            "            Figuring out how to write a daily routine begins by writing down everything you need to get done daily, both at home and at work. Don&rsquo;t worry about how you organize this list; this is a brain dump, not a to-do list. Simply jot down everything you do each day, as well as everything you should get done.<br>\n" +
+            "            Structure Your Day<br>\n" +
+            "            Early birds tend to get things done most effectively before lunchtime while night owls get their burst of energy in the evenings. Think about when you work best. Then, group your tasks into the time of day that makes the most sense for when you will efficiently complete them.<br>\n" +
+            "            Get Specific (Optional)<br>\n" +
+            "            Get as specific as you want with your outline of tasks. For example, write a daily morning routine that looks something like this to account for time:<br>\n" +
+            "            o 6:00: Wake up, shower<br>\n" +
+            "            o 6:30: Breakfast, brush teeth<br>\n" +
+            "            o 7:00: Leave the house<br>\n" +
+            "            o 7:15: Drop off kids at school<br>\n" +
+            "            o 7:30: Arrive at the office</p>\n" +
+            "\n" +
+            "            <p>\n" +
+            "            Schedule Time for Flexibility<br>\n" +
+            "            The goal of creating a daily routine is to harness your most productive hours for challenging tasks and your least productive hours for mundane tasks. But life happens so prepare for unplanned events by keeping some flexible free time within your routine.<br>\n" +
+            "            For instance, you might have a doctor's appointment during your usual work time. Or you have a social gathering at the time you typically prep the next day's lunches. Scheduling blocks of free time into your daily routine helps you shift tasks around while keeping things flowing smoothly in spite of atypical events and you'll never feel pressed for time.<br>\n" +
+            "            Test Drive Your New Routine<br>\n" +
+            "            Once you've created your daily routine, take it for a test drive. Ask yourself:<br>\n" +
+            "            o How does your daily routine feel? <br>\n" +
+            "            o Did you schedule your tasks at times that make sense? <br>\n" +
+            "            o Do you need to adjust anything? <br>\n" +
+            "            Commit to the routine for at least 30 days. Tweak any scheduling that isn't working out. Once your tasks are slotted into the right spots, sticking to your daily routine will be easier.<br>\n" +
+            "            Creating a daily routine can seem daunting, but you will soon reap the rewards when your productivity soars, morning meltdowns are reduced, and you suddenly have pockets of free time. Nothing is written in stone. Keep tweaking as things change in your life. Follow the same steps you used to create your initial routine to form new and productive habits.\n" +
+            "            </p>",
+    }
+};
+
+export function getArticles() {
+    return articles;
+}
