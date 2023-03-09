da9dea5a0fae3c85c9126735a71b7f4f54d81b19
Das, Johannes <Johannes.Das@amplexor.com>
2023-03-09 14:55:53 +0100

    typo

--- src/js/cache.js
+++ src/js/cache.js
@@ -1,6 +1,6 @@
 const writeToCache = (prompt, data) =>
     localStorage.setItem(prompt, JSON.stringify(data));
 
-const readFromCache = promt => JSON.parse(localStorage.getItem(promt)) || null;
+const readFromCache = prompt => JSON.parse(localStorage.getItem(prompt)) || null;
 
 export { readFromCache, writeToCache };
