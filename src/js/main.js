3e83ac88958d185d432131ceeab3ea531dbc9154
Das, Johannes <Johannes.Das@amplexor.com>
2023-03-09 14:47:01 +0100

    use promptGenerator to generate prompt and instructions for text

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,6 @@ import("./loadUser").then((loadUser) => {
                 console.log(prompt)
                 import("./ai").then((ai) => {
                     ai.generateUsingWrapperImage(prompt).then((aiResult) => {
-                        console.log(aiResult);
 
                         if (aiResult) {
                             const imageUrl = aiResult['data']['data'][0]['url'];
@@ -25,28 +24,18 @@ import("./loadUser").then((loadUser) => {
                         }
                     });
                 });
+
+                const textPrompt = promptGenerator.buildTextPrompt(user);
+                const textInstruction = promptGenerator.buildtextInstruction(user);
+                import("./ai").then(function (ai) {
+                    ai.generateUsingWrapperText(textPrompt, textInstruction).then((aiuser) => {
+                        if (aiuser) {
+                            document.getElementById("hero-text").textContent = aiuser["data"]["choices"][0]["text"];
+                        }
+                    });
+                });
             });
         }
-
-        const contentType = "article";
-        const textPrompt = user["subjects"] + " in the style of " + user["artStyles"].join(" ");
-        const textInstruction = "Rewrite " + contentType + " for a " + user["gender"] + "of " + user["age"] + " located in " + user["country"] + " with interests " + user["interests"].join(", ") + " without mentioning the age, country and interests explicitly for the following article";
-
-        console.log(textPrompt);
-        console.log(textInstruction);
-
-        import("./ai").then(function (ai) {
-            ai.generateUsingWrapperText(textPrompt, textInstruction).then((aiuser) => {
-                console.log("user" + aiuser);
-
-                if (aiuser) {
-                    const text = aiuser["data"]["choices"][0]["text"];
-                    console.log("text: " + text);
-
-                    document.getElementById("hero-text").textContent = text;
-                }
-            });
-        });
     });
 });
 
--- src/js/promptGenerator.js
+++ src/js/promptGenerator.js
@@ -28,3 +28,51 @@ export function buildImagePrompt(user) {
 
     return promptParts.join(" ");
 }
+
+export function buildTextPrompt(user) {
+
+    var promptParts = [];
+
+    if (user.subjects) {
+        promptParts = promptParts.concat(user.subjects);
+    }
+
+    if (user.artStyles) {
+        promptParts.push(" in the style of ");
+        promptParts = promptParts.concat(user.artStyles);
+    }
+
+    return promptParts.join(" ");
+}
+
+export function buildtextInstruction(user) {
+    const contentType = "article";
+    var promptParts = [];
+
+    promptParts.push("Rewrite ");
+    promptParts.push(contentType);
+
+    if (user.gender) {
+        promptParts.push("for a");
+        promptParts.push(user.gender);
+    }
+
+    if (user.age) {
+        promptParts.push("of");
+        promptParts.push(user.age);
+    }
+
+    if (user.country) {
+        promptParts.push("located in");
+        promptParts.push(user.country);
+    }
+
+    if (user.interests) {
+        promptParts.push("with interests");
+        promptParts = promptParts.concat(user.interests);
+    }
+
+    promptParts.push("without mentioning the age, country and interests explicitly for the following article");
+
+    return promptParts.join(" ");
+}
