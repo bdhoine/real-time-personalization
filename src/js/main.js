14443e542e5f86a46e2924457152dda309716402
Das, Johannes <Johannes.Das@amplexor.com>
2023-03-09 14:23:26 +0100

    add text generation

--- src/js/main.js
+++ src/js/main.js
@@ -18,6 +18,26 @@ import("./loadUser").then((loadUser) => {
                 });
             });
         }
+
+        const contentType = "article";
+        const textPrompt = user["subjects"] + " in the style of " + user["artStyles"].join(" ");
+        const textInstruction = "Rewrite " + contentType + " for a " + user["gender"] + "of " + user["age"] + " located in " + user["country"] + " with interests " + user["interests"].join(", ") + " without mentioning the age, country and interests explicitly for the following article";
+
+        console.log(textPrompt);
+        console.log(textInstruction);
+
+        import("./ai").then(function (ai) {
+            ai.generateUsingWrapperText(textPrompt, textInstruction).then((aiuser) => {
+                console.log("user" + aiuser);
+
+                if (aiuser) {
+                    const text = aiuser["data"]["choices"][0]["text"];
+                    console.log("text: " + text);
+
+                    document.getElementById("hero-text").textContent = text;
+                }
+            });
+        });
     });
 });
 
